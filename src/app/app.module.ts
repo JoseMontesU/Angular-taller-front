b7256a5ee4983255960328d9cfa5da45cf2798a3
JoseMontesU <josemontes.0@hotmail.com>
2023-01-23 19:02:08 -0500

    comunication between frontend and keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'taller-keycloak',
+        clientId: 'taller-frontend'
+      },
+      initOptions: {
+        // onLoad: 'login-required',
+        onLoad: 'check-sso',
+        // flow: 'standard',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      // loadUserProfileAtStartUp: true
+    });
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +30,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
